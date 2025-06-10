495afbffb3d8a04533e419555f252facc4a553d6
Kaick Gonçalves Flauzino <49240356886@escola927.edu>
2025-06-10 17:24:06 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-//definindo constantes para uso no codigo
-const bntAdd = document.getElementById("bnt-add");
-const tarefa =  document.getElementById("tarefa");
-const tasklist = document.getElementById("tasklist");
-const titulo = document.getElementById("titulo");
+//definir constantes para uso no código
+const btnadd =document.getElementById('btn-add');
+const tarefa = document.getElementById('tarefa')
+const tastlist = document.getElementById('tasklist')
+const titulo = document.getElementById('titulo')
 
-let nome = prompt("Qual o seu nome?");
-
-//if serve para se digitarem vazio fica vazio e nao dá erro
-if (nome){
-    titulo.innerHTML = "Lista de tarefas Generica";
-};
+let nome = prompt("qual o seu nome? ")
+if(nome==""){
+    titulo.innerHTML = "lista de tarefa Genérica"
+}
 else{
-    titulo.innerHTML = `Lista de tarefas: ${nome}`;
-};
-bntAdd.addEventListener("click",criaTarefa);
+titulo.innerHTML = `Lista de tarefa: ${nome}`;
+}
+// acompanha o evento de clique do botão adicionar tarefa
+btnadd.addEventListener("click",criaTarefa);
 
 function criaTarefa(){
+    const listItem =document.createElement('li');
+    listItem.textContent = tarefa.value;
+    tasklist.appendChild(listItem);
+
+
+    //criar botão de deletar tarefa
+    const removeButton = document.createElement('button');
+    removeButton.id ="remove";
+    removeButton.textContent ="X"
+
+    removeButton.addEventListener("click", function(){
+        tasklist.removeChild(listItem)
+    })
+
+
+    //criar botão de concluir tarefa
+    const concluirButton = document.createElement('button');
+    concluirButton.id="conclui";
+    concluirButton.textContent="✓"
+
+    // LISTEitem 
+    let buttonsItem = document.createElement('div');
+    buttonsItem.classList.toggle('buttonsItem')
+    listItem.appendChild(buttonsItem);
+    buttonsItem.appendChild(concluirButton);
+    buttonsItem.appendChild(removeButton);
+
     
+    concluirButton.addEventListener("click",function(){
+        listItem.classList.toggle('completed');
+    })
 }
